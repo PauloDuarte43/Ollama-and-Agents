f2973bba07e1c98e25f3578a2749ebb46ebe8e11
Paulo Duarte <p.roger.antunes43@gmail.com>
2024-06-11 16:32:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,68 +7,46 @@ from langchain_community.tools import DuckDuckGoSearchRun
 search_tool = DuckDuckGoSearchRun()
 
 researcher = Agent(
-  role='Senior Researcher',
-  goal='Uncover groundbreaking technologies in {topic}',
-  verbose=True,
-  memory=True,
-  backstory=(
-    "Driven by curiosity, you're at the forefront of"
-    "innovation, eager to explore and share knowledge that could change"
-    "the world."
-  ),
-  tools=[search_tool],
-  allow_delegation=True
+    role='researcher',
+    goal='Search the internet for the information requested',
+    backstory="""
+    You are a researcher. Using the information in the task, you find out some of the most popular facts about the topic along with some of the trending aspects.
+    You provide a lot of information thereby allowing a choice in the content selected for the final blog.
+    """,
+    verbose=True,
+    allow_delegation=False,
+    tools=[search_tool],
+    llm=ollama_llm
 )
-
 writer = Agent(
-  role='Writer',
-  goal='Narrate compelling tech stories about {topic}',
-  verbose=True,
-  memory=True,
-  backstory=(
-    "With a flair for simplifying complex topics, you craft"
-    "engaging narratives that captivate and educate, bringing new"
-    "discoveries to light in an accessible manner."
-  ),
-  tools=[search_tool],
-  allow_delegation=False
+    role='writer',
+    goal='Craft compelling content on a set of information provided by the researcher.',
+    backstory="""You are a writer known for your humorous but informative way of explaining. 
+    You transform complex concepts into compelling narratives.""",
+    verbose=True,
+    allow_delegation=True,
+    llm=ollama_llm
 )
-
-research_task = Task(
-  description=(
-    "Identify the next big trend in {topic}."
-    "Focus on identifying pros and cons and the overall narrative."
-    "Your final report should clearly articulate the key points,"
-    "its market opportunities, and potential risks."
-  ),
-  expected_output='A comprehensive 3 paragraphs long report on the latest AI trends.',
-  tools=[search_tool],
-  agent=researcher,
+task1 = Task(
+    description="""Research about open source LLMs vs closed source LLMs. 
+    Your final answer MUST be a full analysis report""",
+    agent=researcher,
+    expected_output='A refined finalized version of the blog post in markdown format'
 )
-
-# Writing task with language model configuration
-write_task = Task(
-  description=(
-    "Compose an insightful article on {topic}."
-    "Focus on the latest trends and how it's impacting the industry."
-    "This article should be easy to understand, engaging, and positive."
-  ),
-  expected_output='A 4 paragraph article on {topic} advancements formatted as markdown.',
-  tools=[search_tool],
-  agent=writer,
-  async_execution=False,
-  output_file='new-blog-post.md'
+task2 = Task(
+    description="""Using the insights provided, develop an engaging blog
+    post that highlights the most significant facts and differences between open-source LLMs and closed-source LLMs.
+    Your post should be informative yet accessible, catering to a tech-savvy audience.
+    Make it sound cool, and avoid complex words so it doesn't sound like AI.
+    Your final answer MUST be the full blog post of at least 4 paragraphs.
+    The target word count for the blog post should be between 1,500 and 2,500 words, with a sweet spot at around 2,450 words.""",
+    agent=writer,
+    expected_output='A refined finalized version of the blog post in markdown format'
 )
-
 crew = Crew(
-  agents=[researcher, writer],
-  tasks=[research_task, write_task],
-  process=Process.sequential,
-  memory=True,
-  cache=True,
-  max_rpm=100,
-  share_crew=True
+agents=[researcher, writer],
+tasks=[task1, task2],
+verbose=2,
 )
-
-result = crew.kickoff(inputs={'topic': 'AI in healthcare'})
+result = crew.kickoff()
 print(result)
